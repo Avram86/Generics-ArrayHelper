c5aa31b20fe54b5b19fadc02d19d32a90d43cfc1
Avram86 <60349668+Avram86@users.noreply.github.com>
2021-08-11 11:34:31 +0300

    Add project files.

---
+++ Generics-ArrayHelper/ArrayHelper.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_ArrayHelper
+{
+    public class ArrayHelper<T>
+        where T:IEquatable<T>
+    {
+        public ArrayHelper(T[] array)
+        {
+            Array = array;
+        }
+        public T[] Array { get;}
+
+        public int ReturnIndex(T searchedValue)
+        {
+            int index=0;
+
+            if(searchedValue is null)
+            {
+                index=-1;
+            }
+
+            bool isPresentInArray = Array.Contains(searchedValue);
+
+            if (isPresentInArray)
+            {
+                for(int i = 0; i < Array.Length; i++)
+                {
+                    //implements IEquateble<T> => can use method Equals
+                    if (Array[i].Equals(searchedValue))
+                    {
+                        index = i;
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Elementul {searchedValue} nu este continut in sir.");
+                index=-1;
+            }
+
+            return index;
+        }
+
+        public T[] ReturnArray(int indexStart, int length)
+        {
+            if(indexStart<0 && length < 0)
+            {
+                return new T[0];
+            }
+
+            T[] resultedArray = new T[length];
+
+            int j = 0;
+
+            for(int i = indexStart; i < indexStart+length; i++)
+            {
+                resultedArray[j] = Array[i];
+                j++;
+            }
+
+            return resultedArray;
+        }
+
+    }
+}
---
+++ Generics-ArrayHelper/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Generics_ArrayHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Int Array");
+            ArrayHelper<int> arrayHelper1 = new ArrayHelper<int>(new int[] { 2,8,5,6,3,4,1,10,78});
+            
+            var index= arrayHelper1.ReturnIndex(4);
+            Console.WriteLine($"The position of the element is {index}");
+
+            var array = arrayHelper1.ReturnArray(2,3);
+            Console.Write("The new array is: ");
+            foreach(var item in array)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine("\n");
+
+
+
+            Console.WriteLine("String Array");
+            ArrayHelper<string> arrayHelper2 = new ArrayHelper<string>(new string[] { "abc", "def", "ghi", "jkl", "mno"});
+
+            var index2 = arrayHelper2.ReturnIndex("ghi");
+            Console.WriteLine($"The position of the element is {index2}");
+
+            var array2 = arrayHelper2.ReturnArray(2, 3);
+            Console.Write("The new array is: ");
+            foreach (var item in array2)
+            {
+                Console.Write($"{item}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
